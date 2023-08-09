b84e2661019d89e4ead4c2590a9e3b21db908f7e
Andrew Kirov <AndrewKirov@me.com>
2023-08-09 20:33:18 +0500

    add some sequre

--- kittybot.py
+++ kittybot.py
@@ -1,9 +1,6 @@
-from telegram import Bot, ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
-
-
-BOT_TOKEN = '6496934614:AAFof2bPovzRv2Tprzl6-l-7I6GcoFweMJw'
-
+from config import BOT_TOKEN
 
 updater = Updater(token=BOT_TOKEN)
 
@@ -27,8 +24,3 @@ updater.dispatcher.add_handler(CommandHandler('start', wake_up))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
 updater.start_polling(poll_interval=20.0)
 updater.idle()
-
-# bot = Bot(token=BOT_TOKEN)
-# chat_id = '235620212'
-# text = 'New message!'
-# bot.send_message(chat_id, text)
