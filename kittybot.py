3c79f3bb34d5cb2250afbae691d6334ff4586eb8
Andrew Kirov <AndrewKirov@me.com>
2023-08-09 23:32:00 +0500

    fix url

--- kittybot.py
+++ kittybot.py
@@ -19,7 +19,7 @@ def get_random_cat_image():
         new_url = 'https://api.thedogapi.com/v1/images/search'
         response = requests.get(new_url)
 
-    response = requests.get(URL).json()
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
